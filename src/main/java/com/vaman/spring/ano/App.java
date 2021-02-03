c1b747782a4045e874a5e5db2114cb5f3bee7c13
Vaman Deshmukh <vamandeshmukh@gmail.com>
2021-02-03 20:40:41 +0530

    Create App.java

---
+++ src/main/java/com/vaman/spring/ano/App.java
@@ -0,0 +1,13 @@
+package com.vaman.spring.ano;
+
+/**
+ * 
+ * @author Vaman Deshmukh
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Start");
+	}
+
+}
